747d516a1a1490ff98ec64b771cb83cd80d060d0
Shivang Singh <138879342+shivangyash@users.noreply.github.com>
2023-08-28 13:04:51 +0530

    Update check.py

--- check.py
+++ check.py
@@ -34,10 +34,10 @@ if st.session_state.df is None:
 submitted = st.button("Submit")
 if submitted:
     with st.spinner():
-        llm = OpenAI(api_token=st.secrets["openai_api_key"])
+        llm = OpenAI(api_token=API_KEY)
         pandas_ai = PandasAI(llm, middlewares=[StreamlitMiddleware()], verbose=False)
-        question = question = '''remove extra spaces from all the column names,  remove % from the column age of borrower, subtract 1 month from the start date column,
-             remove the british sterling pound symbol from columns (amount financed, fixed contractual repayment, capital received), subtract 1 from every value in age of borrower column, replace the values of column remaining term_v1 with the value come after subtracting 6 from value in term,
+        question = '''remove extra spaces from all the column names, convert dates into 'yyyy-mm-dd' format,
+             convert the column amount financed, fixed contractual repayment, interest received, capital received, current principe o/s, arrears amount to numeric type by replacing , and pound symbol to '', replace the values of column remaining term_v1 with the value come after subtracting 6 from value in term,
               convert column ('Case Status Live or Arrears', 'Client >=21 years Old', 'Min Balance €2,000 and Max Balance €8,000', 'Term Max 60 mths', 'Monthly Arrears Payments <=2', 'Weekly Arrears Payments <=5', 'Weekly Impaired Loans: DD <63 days old 40%', 'Monthly impaired loans DD<180 days 40%') into boolean type that is 'FALSE' if original value is 0 and 'TRUE' if it is 1'''
         x = pandas_ai.run(st.session_state.df, prompt=question, is_conversational_answer=False)
         # st.write(x)
@@ -51,6 +51,7 @@ if st.session_state.df is not None:
     st.write(st.session_state.df)
     # dff = st.session_state.df
     if submitted:
+        st.write('Cleaned dataframe')
         st.session_state.df.to_csv('latest.csv')
         st.download_button(label='Download Cleaned File', data=st.session_state.df.to_csv(), mime='text/csv')
 
